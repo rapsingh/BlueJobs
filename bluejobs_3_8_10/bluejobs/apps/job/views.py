bc6cdcfef4342e4a8d60d4a1af61634cea64e8f8
Rana Aishwarya Pratap Singh <67787691+rapsingh@users.noreply.github.com>
2022-07-23 04:45:57 +0530

    ADDING JOBS

--- bluejobs_3_8_10/bluejobs/apps/job/views.py
+++ bluejobs_3_8_10/bluejobs/apps/job/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+
+from .models import Job
+
+
+
+def job_detail(request, job_id):
+    job = Job.objects.get(pk=job_id)
+
+    return render(request, 'job/job_detail.html', {'job': job})
+
--- bluejobs_3_8_10/bluejobs/bluejobs/urls.py
+++ bluejobs_3_8_10/bluejobs/bluejobs/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views
 
 
 from apps.core.views import frontpage, signup
+from apps.job.views import job_detail
 
 urlpatterns = [
     path('',frontpage, name='frontpage'),
@@ -28,4 +29,7 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     path('logout/',views.LogoutView.as_view() , name='logout'),
     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+
+    path('jobs/<int:job_id>/', job_detail, name = 'job_detail'),
+
 ]
